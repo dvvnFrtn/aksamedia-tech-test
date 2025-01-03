b4da2f9f6316d1416d2d59b29012aad0e4c67300
dvvnFrtn <muhrizkifajar28@gmail.com>
2025-01-02 23:35:05 -0500

    standarize api response format

---
+++ app/Helpers/ApiResponse.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Helpers;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ApiResponse
+{
+    public static function success(
+        $data = null, 
+        $message = null, 
+        $code = Response::HTTP_OK
+    )
+    {
+        return response()->json([
+            'status' => 'success',
+            'message' => $message,
+            'data' => $data
+        ], $code);
+    }
+
+    public static function error(
+        $exCode, 
+        $message, 
+        $description, 
+        $code = Response::HTTP_BAD_REQUEST
+    )
+    {
+        return response()->json([
+            'status' => 'error',
+            'code' => $exCode,
+            'message' => $message,
+            'description' => $description,
+        ], $code);
+    }
+
+    public static function validationError($errors)
+    {
+        return response()->json([
+            'status' => 'error',
+            'message' => 'The request payload contains failed validation',
+            'errors' => $errors
+        ], Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+}
